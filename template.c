b9d56f88e688fb60036f31a5ea82713fb089a1e0
David Ward <davidmward@mail.com>
2023-06-05 20:21:02 -0700

    added template file

---
+++ template.c
@@ -0,0 +1,17 @@
+// Standard header files
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+// SDL header files
+#include <SDL2/SDL_surface.h>
+#include <SDL2/SDL_events.h>
+#include <SDL2/SDL_video.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+
+int main(int argc, char **argv)
+{
+    return 0;
+}
